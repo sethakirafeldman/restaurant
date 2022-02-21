425ad1e7dbcf00fe2a20b70ced4fcdf02990cc7b
Seth Feldman <namdlefhtes@gmail.com>
2022-02-21 10:09:22 -0500

    Good mobile responsiveness

--- src/contact.js
+++ src/contact.js
@@ -6,7 +6,7 @@ const contactInfo = {
     
     phone: "<a href='tel:555-555-5555'>555-555-5555</a>",
     email: "<a href= 'mailto: contact@zeebistro.com'>contact@zeebistro.com</a>",
-    location: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2646.4152322667064!2d-79.39090632127413!3d43.670295135942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34af9fd214e7%3A0x4df168bf23766a19!2sPusateri&#39;s%20Fine%20Foods!5e0!3m2!1sen!2sca!4v1626799589253!5m2!1sen!2sca" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'
+    location: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5889.785943386578!2d-82.21936480529077!3d42.43001299315603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882557e3cd85185b%3A0x768b8fd1f4d30c4a!2sZee&#39;s%20Bistro!5e0!3m2!1sen!2sca!4v1645456030583!5m2!1sen!2sca" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'
 
 };
 
